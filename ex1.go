1d661d4668cd64287a6b18b53c41c9f47252c881
Kobayashi-213 <b170170@icloud.com>
2021-06-23 02:37:25 +0000

    all test pass

--- ex1.go
+++ ex1.go
@@ -48,6 +48,54 @@ func getHundred(n int) string {
 	}
 	return str
 }
+func getTen(n int) string {
+	ten := (int)(n / 10)
+	str := ""
+	if ten == 1 {
+		str = "X"
+	} else if ten == 2 {
+		str = "XX"
+	} else if ten == 3 {
+		str = "XXX"
+	} else if ten == 4 {
+		str = "XL"
+	} else if ten == 5 {
+		str = "L"
+	} else if ten == 6 {
+		str = "LX"
+	} else if ten == 7 {
+		str = "LXX"
+	} else if ten == 8 {
+		str = "LXXX"
+	} else if ten == 9 {
+		str = "XC"
+	}
+	return str
+}
+func getOne(n int) string {
+	one := (int)(n / 1)
+	str := ""
+	if one == 1 {
+		str = "I"
+	} else if one == 2 {
+		str = "II"
+	} else if one == 3 {
+		str = "III"
+	} else if one == 4 {
+		str = "IV"
+	} else if one == 5 {
+		str = "V"
+	} else if one == 6 {
+		str = "VI"
+	} else if one == 7 {
+		str = "VII"
+	} else if one == 8 {
+		str = "VIII"
+	} else if one == 9 {
+		str = "IX"
+	}
+	return str
+}
 
 func solve(n int) string {
 	//千のくらいについてローマ数字を返す
@@ -56,8 +104,12 @@ func solve(n int) string {
 	hnum := calcu(n, 1000)
 
 	hundRedStr := getHundred(hnum)
+	tnum := calcu(n, 100)
+	tenStr := getTen(tnum)
 
-	return thouSandStr + hundRedStr
+	onum := calcu(n, 10)
+	oneStr := getOne(onum)
+	return thouSandStr + hundRedStr + tenStr + oneStr
 }
 
 func calcu(n int, m int) int {
@@ -65,5 +117,5 @@ func calcu(n int, m int) int {
 }
 
 func main() {
-	solve(1500)
+	//fmt.Println(solve(1900))
 }
--- ex1_test.go
+++ ex1_test.go
@@ -18,18 +18,16 @@ func Test02(t *testing.T) {
 }
 
 func Test03(t *testing.T) {
-	result := solve(1990)
+	result := solve(1900)
 	expected := "MCM"
 	if result != expected {
 		t.Error("Test03 is failed")
 	}
 }
-
-/*func Test04(t *testing.T) {
-	result := calcu(3000)
-	result2 := numToStr(result)
-	expected := "MMM"
+func Test04(t *testing.T) {
+	result := solve(1999)
+	expected := "MCMXCIX"
 	if result != expected {
-		t.Error("Test03 is failed")
+		t.Error("Test04 is failed")
 	}
-}*/
+}
